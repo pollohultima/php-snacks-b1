2deb92b9ef59763a743768ea34d8598607513c1a
pollohultima <matteo.polonio@gmail.com>
2021-12-02 17:35:43 +0100

    Snack_7 complete

--- snack_7/index.php
+++ snack_7/index.php
@@ -1,5 +1,36 @@
 <?php
 
+/* Snack 7
+Creare un array contenente qualche alunno di un’ipotetica classe. Ogni alunno avrà Nome, Cognome e un array contenente i suoi voti scolastici. Stampare Nome, Cognome e la media dei voti di ogni alunno. */
+
+$students=[
+    [
+        'name' => 'Mattia',
+        'lastName' => 'Pollato',
+        'votes' => [
+            16,15,20,29
+        ]
+        ],
+
+        [
+        'name' => 'Andrea',
+        'lastName' => 'Cisello',
+        'votes' => [
+            19,18,20,21
+        ]
+        ],
+
+         [
+        'name' => 'Mario',
+        'lastName' => 'Grande',
+        'votes' => [
+            16,25,20,28
+        ]
+    ]
+]
+
+
+
 
 
 ?>
@@ -14,5 +45,17 @@
 </head>
 <body>
     
+
+<?php
+for ($i=0; $i < count($students); $i++) {  ?>
+      
+    <p> <?= $students[$i]['name']; ?> 
+     <?= $students[$i]['lastName']; ?> </p> 
+    <p>La media dei voti è:
+    <?= round(array_sum($students[$i]['votes'])/count($students[$i]['votes'])); ?></p>
+
+ 
+     
+<?php } ?>
 </body>
 </html>
