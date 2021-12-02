4c7722cc78d7295ba3813fa5cf33a76a1ad82080
pollohultima <matteo.polonio@gmail.com>
2021-12-02 17:15:01 +0100

    Snack_666 complete

--- snack_6/index.php
+++ snack_6/index.php
@@ -1,6 +1,30 @@
 <?php
 
+/* Snack 6
+Utilizzare questo array: https://pastebin.com/CkX3680A. Stampiamo il nostro array mettendo gli insegnanti in un rettangolo grigio e i PM in un rettangolo verde. */
 
+  $db = [
+        'teachers' => [
+            [
+                'name' => 'Michele',
+                'lastname' => 'Papagni'
+            ],
+            [
+                'name' => 'Fabio',
+                'lastname' => 'Forghieri'
+            ]
+        ],
+        'pm' => [
+            [
+                'name' => 'Roberto',
+                'lastname' => 'Marazzini'
+            ],
+            [
+                'name' => 'Federico',
+                'lastname' => 'Pellegrini'
+            ]
+        ]
+    ];
 
 ?>
 
@@ -13,6 +37,32 @@
     <title>Document</title>
 </head>
 <body>
-    
+ 
+<div class="teachers" style="background-color: gray;">  
+<h3>Teachers</h3>
+
+<?php
+for ($i=0; $i < count($db['teachers']); $i++) {  ?>
+   <p>  
+    <?= $db['teachers'][$i]['name']; ?> 
+    <?= $db['teachers'][$i]['lastname']; ?>
+</p>  
+     
+<?php } ?>
+</div>
+
+<div class="pm" style="background-color: green;">
+<h3>Pm</h3>
+
+<?php
+for ($i=0; $i < count($db['pm']); $i++) {  ?>
+   <p>  
+    <?= $db['pm'][$i]['name']; ?> 
+    <?= $db['pm'][$i]['lastname']; ?>
+</p>  
+     
+<?php } ?>
+</div>
+
 </body>
 </html>
