ff21590a69b609784d86d8ebe7afb156e5956ea2
pollohultima <matteo.polonio@gmail.com>
2021-12-03 12:38:49 +0100

    condition doesn't work

--- blocco_2/snack_1/index.php
+++ blocco_2/snack_1/index.php
@@ -35,9 +35,6 @@ $ads = [
 
 
 
-
-
-
 ?>
 
 <!DOCTYPE html>
---
+++ blocco_2/snack_2/index.php
@@ -0,0 +1,29 @@
+<?php
+
+/* Snack 2
+Creiamo due pagine php. La prima farà una chiamata GET alla seconda attraverso un form. Il form avrà un input di testo in cui inserire il nome di un invitato. Inseriamo nell’altro file una lista di invitati. Se la nostra query è contenuta nella lista degli invitati stampare ok, altrimenti ko. */
+
+
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+        
+    </style>
+</head>
+<body>
+    
+
+
+    
+
+</body>
+</html>
